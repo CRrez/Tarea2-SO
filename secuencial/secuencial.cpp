45d62e1e55f2ad9915d0a4198ff9d3ee7d17eb1b
crrez <cristobalramirez79@gmail.com>
2023-12-03 15:57:27 -0300

    imagen y arreglo en codigo

--- secuencial/secuencial.cpp
+++ secuencial/secuencial.cpp
@@ -5,25 +5,31 @@ using namespace cv;
 using namespace std;
 using namespace std::chrono;
 
-// read the image file .
-Mat image = imread ("imagenacolor.jpg",IMREAD_COLOR) ;
-// read each pixel (RGB pixel) .
+// lee la imagen
+Mat image = imread ("imagenacolor.png",IMREAD_COLOR) ;
+
 int main()
 {
-
+        //inicia el temporizador
         auto start_time = high_resolution_clock::now();
 
             for (int r =0; r<image.rows; r++) 
             {
                 for (int c =0; c<image.cols; c++) 
                 {
+
+                    //saca un pixel de la imagen,obteniendo su patron RGB
                     Vec3b *p =image.ptr<Vec3b>(r,c);
                     
+                    //aplica la escal de grises con el formato BGR
                     int grayscaleValue = 0.3 * (*p)[2] + 0.59 * (*p)[1] + 0.11 * (*p)[0];
+                    //implementa el nuevo pixel en escala de grises
                     image.at<uchar>(r, c) = static_cast<uchar>(grayscaleValue);
 
                 }
             }
+
+            //termina el temporizador
              auto end_time = high_resolution_clock::now();
              auto duration = duration_cast<milliseconds>(end_time - start_time);
 
