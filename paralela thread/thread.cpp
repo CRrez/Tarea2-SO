9e2a0bdb2762ebd0ad122b5808cc602ca82b11b6
crrez <cristobalramirez79@gmail.com>
2023-12-03 11:55:43 -0300

    cambio thread

--- paralela thread/thread.cpp
+++ paralela thread/thread.cpp
@@ -11,7 +11,7 @@ int main()
 for (int r =0; r<image.rows; r++) 
 {
     for (int c =0; c<image.cols; c++) {
-        Point3 <uchar >∗p =image.ptr<Point3 <uchar> >(r,c) ;
+        Point3 *p =image.ptr<Point3 <uchar> >(r,c) ;
         //x:B y:G z:R
         printf ("( %d %d %d )" ,p−>x,p−>y,p−>z) ;
     }
