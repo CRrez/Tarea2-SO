e236eb8966e32dd6d278e0463816799e9b410cbf
crrez <cristobalramirez79@gmail.com>
2023-12-03 16:19:55 -0300

    comentarios agregados

--- paralela open/ope.cpp
+++ paralela open/ope.cpp
@@ -7,24 +7,31 @@ using namespace std;
 using namespace std::chrono;
 
 int main() {
+    //lectura de la imagen
     Mat image = imread("imagenacolor.png", IMREAD_COLOR);
 
+    //numero de hebras con las que se trabajara
     int num_threads;
 
+    //inicio del temporizador
     auto start_time = high_resolution_clock::now();
+    //aca se indica de que el for trabajara en base a las cantidades de hebras solicitadas
 
     #pragma omp parallel for num_threads(num_threads)
-    for (int r = 0; r < image.rows; ++r) {
-        for (int c = 0; c < image.cols; ++c) {
+    for (int r = 0; r < image.rows; ++r) 
+    {
+        for (int c = 0; c < image.cols; ++c) 
+        {
             Vec3b *p = image.ptr<Vec3b>(r, c);
             int grayscaleValue = 0.3 * (*p)[2] + 0.59 * (*p)[1] + 0.11 * (*p)[0];
             image.at<uchar>(r, c) = static_cast<uchar>(grayscaleValue);
         }
     }
-
+    //fin del temporizador
     auto end_time = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(end_time - start_time);
 
+    //impresion del a imagen en la escal de grises
     imwrite("imagengris.png", image);
     return 0;
 }
