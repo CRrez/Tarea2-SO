efc1169d160b415570cc11055b76cebe6569a40c
crrez <cristobalramirez79@gmail.com>
2023-12-03 12:34:35 -0300

    libreria chrono

--- paralela open/ope.cpp
+++ paralela open/ope.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-using namespace cv ;
-using namespace s t d ;
+#include <chrono>
+using namespace cv;
+using namespace std;
 // read the image file .
-Mat image = imread ( ”imagenacolor.jpg”,IMREAD COLOR) ;
+Mat image = imread ("imagenacolor.jpg",IMREAD_COLOR) ;
 // read each pixel (RGB pixel) .
-for (int r =0; r<image.rows; r++) 
+int main()
 {
-        for (int c =0; c<image.cols; c++) 
-        {
-            Point3 <uchar >∗p =image.ptr<Point3 <uchar> >(r,c) ;
-            //x:B y:G z:R
-            printf ("( %d %d %d )" ,p−>x,p−>y,p−>z) ;
-        }   
+            for (int r =0; r<image.rows; r++) 
+            {
+                for (int c =0; c<image.cols; c++) 
+                {
+                    Vec3b *p =image.ptr<Vec3b>(r,c);
+                    //x:B y:G z:R
+                    printf ("( %d %d %d )" , (*p)[0],(*p)[1],(*p)[2]) ;
+                }
+            }
 }
