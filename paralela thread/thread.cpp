0f283f6a2e050ad7fe563f0b358cfe36f7867299
crrez <cristobalramirez79@gmail.com>
2023-12-03 11:48:42 -0300

    solo programa thread

--- paralela thread/thread.cpp
+++ paralela thread/thread.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 using namespace cv ;
-using namespace s t d ;
+using namespace std ;
 // read the image file .
 Mat image = imread ( ”imagenacolor.jpg”,IMREAD COLOR) ;
 // read each pixel (RGB pixel) .
+
+int main()
+{
 for (int r =0; r<image.rows; r++) 
 {
     for (int c =0; c<image.cols; c++) {
@@ -12,4 +15,5 @@ for (int r =0; r<image.rows; r++)
         //x:B y:G z:R
         printf ("( %d %d %d )" ,p−>x,p−>y,p−>z) ;
     }
+}
 }
