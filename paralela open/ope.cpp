9c9bad61e78a1737ae0c0ef089521ae3efeb31c3
crrez <cristobalramirez79@gmail.com>
2023-12-03 15:08:30 -0300

    aplicado cantidad de hebras

--- paralela open/ope.cpp
+++ paralela open/ope.cpp
@@ -6,23 +6,27 @@ using namespace cv;
 using namespace std;
 using namespace std::chrono;
 
-// read the image file .
-Mat image = imread ("imagenacolor.jpg",IMREAD_COLOR) ;
-// read each pixel (RGB pixel) .
-int main()
-{
+int main() {
+    Mat image = imread("imagenacolor.jpg", IMREAD_COLOR);
 
-        auto start_time = high_resolution_clock::now();
+    int num_threads;
 
-            for (int r =0; r<image.rows; r++) 
-            {
-                for (int c =0; c<image.cols; c++) 
-                {
-                    Vec3b *p =image.ptr<Vec3b>(r,c);
-                    //x:B y:G z:R
-                    printf ("( %d %d %d )" , (*p)[0],(*p)[1],(*p)[2]) ;
-                }
-            }
-             auto end_time = high_resolution_clock::now();
-             auto duration = duration_cast<milliseconds>(end_time - start_time);
+    auto start_time = high_resolution_clock::now();
+
+    #pragma omp parallel for num_threads(num_threads)
+    for (int r = 0; r < image.rows; ++r) {
+        for (int c = 0; c < image.cols; ++c) {
+            Vec3b *p = image.ptr<Vec3b>(r, c);
+            int grayscaleValue = 0.3 * (*p)[2] + 0.59 * (*p)[1] + 0.11 * (*p)[0];
+            image.at<uchar>(r, c) = static_cast<uchar>(grayscaleValue);
+        }
+    }
+
+    auto end_time = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end_time - start_time);
+
+    imshow("Grayscale Image", image);
+    waitKey(0);
+
+    return 0;
 }
